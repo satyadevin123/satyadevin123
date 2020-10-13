113b1ae5b9ad99b6dcec61ede41cd9b9443ff3c7
Satyadevi Nimmakayala <Satyadevi.Nimmakayala@WinWire.com>
2020-10-13 17:11:04 +0530

    removed the backup tables

--- Metadata DB/Metadata DB/dbo/Tables/T_List_DataSets_bkup.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[T_List_DataSets_bkup] (
-    [id]               INT            NOT NULL,
-    [DataSet_name]     NVARCHAR (255) NULL,
-    [LinkedService_id] INT            NULL,
-    [created_date]     DATE           NULL,
-    [Jsoncode]         VARCHAR (8000) NULL
-);
-
--- Metadata DB/Metadata DB/dbo/Tables/t_list_datasets_bkup2.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[t_list_datasets_bkup2] (
-    [id]                  INT            NOT NULL,
-    [DataSet_name]        NVARCHAR (255) NULL,
-    [LinkedService_id]    INT            NULL,
-    [created_date]        DATE           NULL,
-    [Jsoncode]            VARCHAR (8000) NULL,
-    [DataSetStandardName] NVARCHAR (200) NULL
-);
-
